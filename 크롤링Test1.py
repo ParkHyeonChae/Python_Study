53f2e940075911815590a4c42a9ab357e2763cc5
ParkHyeonChae <52586888+ParkHyeonChae@users.noreply.github.com>
2019-07-28 17:21:22 +0900

    네이버사이트 실시간검색어 스크래핑

--- 크롤링Test1.py
+++ 크롤링Test1.py
@@ -8,7 +8,7 @@ html = req.text # 요청한 사이트의 url의 html내용을 가져온다
 soup = bs(html, 'html.parser') # 파싱
 newest = soup.select('span.ah_k') # ah_k인 태그를 selector로 지정
 
-count=0
+count=0 # 20위까지 표시를 위해
 for news in newest:
     count+=1
     print(str(count)+"위 : "+news.text)
